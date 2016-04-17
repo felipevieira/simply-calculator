ac9e5bdc653e2547eb18550744c7563be014d34f
Felipe Vieira <felipe29vieira@gmail.com>
2016-04-17 01:57:45 -0300

    fixing negative exponentiation and some style files

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,32 @@
 function appendSymbol(symbol) {
+	if (document.getElementById('expression-panel').style.fontWeight.localeCompare('bold') == 0) {
+		document.getElementById('expression-panel').style.fontWeight = 'normal'
+		clearDisplay()
+	}
 	document.getElementById('expression-panel').value = document.getElementById('expression-panel').value + symbol;
 }
 
 function evaluateExpression() {
 	try {
-		result = eval(normalizeExpression(document.getElementById('expression-panel').value))
+		// Trying to evaluate de expression result
+		result = eval(normalizeExpression(document.getElementById('expression-panel').value))		
 		document.getElementById('expression-panel').value = result
 	}
 	catch(err) {
-	    document.getElementById('expression-panel').value = "Malformed Expression"
-	}	
+	    document.getElementById('expression-panel').value = 'Malformed Expression'
+	}
+
+	document.getElementById('expression-panel').style.fontWeight = 'bold';
 }
 
 function normalizeExpression(expression) {
 	// Replacing multiplication and division symbols
 	expression = expression.replace(/\u00F7/g,'/')
 	expression = expression.replace(/\u00D7/g,'*')
+	expression = expression.replace(/,/g,'.')
 
 	// Replacing exponentiation symbols
-	expression = expression.replace(/[0-9](\^[0-9])+/g, function myFunction(exponentialExpression){
+	expression = expression.replace(/\-?[0-9](\^\-?[0-9])+/g, function myFunction(exponentialExpression){
 		exponents = exponentialExpression.split("^")
 		currentValue = Math.pow(exponents[exponents.length-2], exponents[exponents.length-1])
 		for (i = exponents.length - 3; i >= 0; i--) {
@@ -28,5 +36,19 @@ function normalizeExpression(expression) {
 	});
 
 	return expression
+}
+
+function clearDisplay() {
+	document.getElementById('expression-panel').value = ''
+}
+
+function keyboardEvent(e) {	
+    keyCode = e.keyCode || e.which;
+    if (keyCode == '13'){
+      evaluateExpression()
+    }
+    else {
+    	appendSymbol('')
+    }
 }
 
